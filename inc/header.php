fa7960357b1e689c5369a09b6ce6ddc2c0803489
Nichole Dwight <nicholedwight@gmail.com>
2015-04-07 20:12:41 +0100

    working on off canvas menu

---
+++ inc/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Action on Disability and Work UK</title>
+    <link rel="stylesheet" href="./assets/css/main.css">
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include('inc/header.php'); ?>
+  <input type="checkbox" id="nav-trigger" class="nav-trigger" />
+  <label for="nav-trigger"></label>
+
+<header class="header">
+  <h1>TEST HEADER</h1>
+</header>
+
+<nav id="nav" role="navigation">
+  <ul class="navigation">
+    <li class="nav-item"><a href="#">Home</a></li>
+    <li class="nav-item"><a href="#">Portfolio</a></li>
+    <li class="nav-item"><a href="#">About</a></li>
+    <li class="nav-item"><a href="#">Blog</a></li>
+    <li class="nav-item"><a href="#">Contact</a></li>
+  </ul>
+</nav>
+
+<div id="main" role="main" class="site-wrap">
+
+</div>
+
+<footer>
+
+</footer>
+
+
+<script src="./assets/js/main.js"></script>
+
+</body>
+</html>
